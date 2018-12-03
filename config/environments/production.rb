d92d1308a75b24bc1b144f71e09a64b2e2a29ea7
skarimo <karimovj@gmail.com>
2018-12-03 01:26:39 -0500

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.web_socket_server_url = "wss://stock-trainer-backend.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://stock-trainer-backend.herokuapp.com/cable', 'http://stock-trainer-backend.herokuapp.com/cable']
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
